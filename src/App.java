378a81ddd04747fac3d06e0649a9bbaa720d9e50
Christian Astudillo <chris.astu134@gmail.com>
2024-10-16 12:51:16 -0500

    Proyecto Final

--- src/App.java
+++ src/App.java
@@ -5,24 +5,44 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         ArrayList<Persona> listaPersonas = new ArrayList<>();
+        int cantidadPersonas;
 
-
-        System.out.print("Ingrese la cantidad de personas del listado: ");
-        int cantidadPersonas = sc.nextInt();
+        do {
+            System.out.print("Ingrese la cantidad de personas del listado: ");
+            cantidadPersonas = sc.nextInt();
+            if (cantidadPersonas < 0) {
+                System.out.println("Error: La cantidad de personas no puede ser negativa.");
+            }
+        } while (cantidadPersonas < 0);
 
         for (int i = 0; i < cantidadPersonas; i++) {
-            System.out.println("Ingrese Persona: ");
+            System.out.println("Ingrese Persona " + (i + 1) + ":");
+
             System.out.print("Nombre: ");
             String nombre = sc.next();
-            System.out.print("Edad: ");
-            int edad = sc.nextInt();
+
+            int edad;
+            do {
+                System.out.print("Edad: ");
+                edad = sc.nextInt();
+                if (edad < 0) {
+                    System.out.println("Error: La edad no puede ser negativa.");
+                }
+            } while (edad < 0);
+
             listaPersonas.add(new Persona(nombre, edad));
         }
 
         Busqueda.ordenarPorInsercion(listaPersonas);
 
-        System.out.print("Ingrese la edad a buscar: ");
-        int edadBuscada = sc.nextInt();
+        int edadBuscada;
+        do {
+            System.out.print("Ingrese la edad a buscar: ");
+            edadBuscada = sc.nextInt();
+            if (edadBuscada < 0) {
+                System.out.println("Error: La edad no puede ser negativa.");
+            }
+        } while (edadBuscada < 0);
 
         Busqueda busqueda = new Busqueda();
         int posicion = busqueda.busquedaBinaria(listaPersonas, edadBuscada);
@@ -32,6 +52,7 @@ public class App {
         } else {
             System.out.println("No se encontró una persona con la edad " + edadBuscada);
         }
+
         sc.close();
     }
 }
--- src/Busqueda.java
+++ src/Busqueda.java
@@ -7,18 +7,22 @@ public class Busqueda {
         while (bajo <= alto) {
             int centro = (bajo + alto) / 2;
             Persona personaCentro = lista.get(centro);
-            for (Persona p : lista) {
-                System.out.print(p.getEdad() + " ");
+            // for (Persona p : lista) {
+            //     System.out.print(p.getEdad() + " ");
+            // }
+            for (int i = bajo; i <= alto; i++) {
+                System.out.print(lista.get(i).getEdad() + " | ");
             }
-            System.out.println("\nbajo=" + bajo + " alto=" + alto + " centro=" + centro + " valorCentro=" + personaCentro.getEdad());
+            System.out.print("\nbajo=" + bajo + " alto=" + alto + " centro=" + centro + " valorCentro=" + personaCentro.getEdad());
+            System.out.println(" ");
 
             if (personaCentro.getEdad() == valorBuscado) {
                 return centro; 
             } else if (personaCentro.getEdad() < valorBuscado) {
-                System.out.println("--> Derecha");
+                System.out.println(" --> Derecha");
                 bajo = centro + 1;
             } else {
-                System.out.println("-->Izquierda");
+                System.out.println(" -->Izquierda");
                 alto = centro - 1;
             }
         }
